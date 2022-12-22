724cf753a9c4ebbb0f07172a533a66c69aae59b1
Kyryl5 <108937818+Kyryl5@users.noreply.github.com>
2022-12-22 15:20:43 +0200

    add loaders (#2)
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>

--- src/components/CocktailsList.jsx
+++ src/components/CocktailsList.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function CocktailsList() {
 	const [cocktailName, setCocktailName] = useState("");
 	const [searchedCocktails, setSearchedCocktails] = useState([]);
 	const [error, setError] = useState(false);
-	const [alcoCocktails, setAlcoCocktails] = useState([]);
-	const [nonAlcoCocktails, setNonAlcoCocktails] = useState([]);
-	const [randomCocktail, setRandomCocktail] = useState([]);
+	// const [alcoCocktails, setAlcoCocktails] = useState([]);
+	// const [nonAlcoCocktails, setNonAlcoCocktails] = useState([]);
+	// const [randomCocktail, setRandomCocktail] = useState([]);
 
 	const searchByName = (e) => {
 		e.preventDefault();
@@ -28,48 +29,39 @@ export default function CocktailsList() {
 				});
 		}
 	};
-	const alcoholicCocktails = () => {
-		fetch(
-			"https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
-		).then((res) => res.json().then((res) => setAlcoCocktails(res.drinks)));
-	};
-	const nonAlcoholicCocktails = () => {
-		fetch(
-			"https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
-		).then((res) => res.json().then((res) => setNonAlcoCocktails(res.drinks)));
-	};
+	// const alcoholicCocktails = () => {
+	// 	fetch(
+	// 		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+	// 	).then((res) => res.json().then((res) => setAlcoCocktails(res.drinks)));
+	// };
+	// const nonAlcoholicCocktails = () => {
+	// 	fetch(
+	// 		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+	// 	).then((res) => res.json().then((res) => setNonAlcoCocktails(res.drinks)));
+	// };
 
-	const searchRandomCocktail = () => {
-		fetch(
-			"https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
-		).then((res) => res.json().then((res) => setRandomCocktail(res.drinks)));
-		fetch(
-			"https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
-		).then((res) =>
-			res
-				.json()
-				.then((res) =>
-					setRandomCocktail((prev) =>
-						[...prev, ...res.drinks]
-							.sort(() => Math.random() - 0.7)
-							.splice(0, 9)
-					)
-				)
-		);
-	};
+	// const searchRandomCocktail = () => {
+	// 	fetch(
+	// 		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+	// 	).then((res) => res.json().then((res) => setRandomCocktail(res.drinks)));
+	// 	fetch(
+	// 		"https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+	// 	).then((res) =>
+	// 		res
+	// 			.json()
+	// 			.then((res) =>
+	// 				setRandomCocktail((prev) =>
+	// 					[...prev, ...res.drinks]
+	// 						.sort(() => Math.random() - 0.7)
+	// 						.splice(0, 9)
+	// 				)
+	// 			)
+	// 	);
+	// };
 	return (
 		<div className="cocktails">
 			<h1>COCKTAILS</h1>
-			<button
-				onClick={() => {
-					setSearchedCocktails([]);
-					setAlcoCocktails([]);
-					setNonAlcoCocktails([]);
-					setRandomCocktail([]);
-				}}
-			>
-				Clear
-			</button>
+			<button>Clear</button>
 			<div className="cocktails_search">
 				<form
 					onSubmit={(e) => {
@@ -94,52 +86,19 @@ export default function CocktailsList() {
 				</form>
 			</div>
 			<div className="navigation">
-				<button onClick={searchRandomCocktail}>Random Cocktails</button>
-				<button onClick={alcoholicCocktails}>Alcoholic</button>
-				<button onClick={nonAlcoholicCocktails}>Non-Alcoholic</button>
+				<NavLink to="/games-cocktails-app/cocktails/random">
+					<button>Random Cocktails</button>
+				</NavLink>
+
+				<NavLink to="/games-cocktails-app/cocktails/alcoholic">
+					<button>Alcoholic</button>
+				</NavLink>
+				<NavLink to="/games-cocktails-app/cocktails/non-alcoholic">
+					<button>Non-Alcoholic</button>
+				</NavLink>
 			</div>
-			<div className="Random_Cocktails">
-				{randomCocktail?.map((el) => (
-					<div className="cocktail" key={el.idDrink}>
-						<div className="cocktail_image">
-							<img
-								style={{ width: "200px", height: "200px" }}
-								src={el.strDrinkThumb}
-								alt=""
-							/>
-						</div>
-						<div className="cocktail_name">{el.strDrink}</div>
-					</div>
-				))}
-			</div>
-			<div className="Alcoholic">
-				{alcoCocktails?.map((el) => (
-					<div className="cocktail" key={el.idDrink}>
-						<div className="cocktail_image">
-							<img
-								style={{ width: "200px", height: "200px" }}
-								src={el.strDrinkThumb}
-								alt=""
-							/>
-						</div>
-						<div className="cocktail_name">{el.strDrink}</div>
-					</div>
-				))}
-			</div>
-			<div className="Non-Alcoholic">
-				{nonAlcoCocktails?.map((el) => (
-					<div className="cocktail" key={el.idDrink}>
-						<div className="cocktail_image">
-							<img
-								style={{ width: "200px", height: "200px" }}
-								src={el.strDrinkThumb}
-								alt=""
-							/>
-						</div>
-						<div className="cocktail_name">{el.strDrink}</div>
-					</div>
-				))}
-			</div>
+			<Outlet></Outlet>
+
 			<div className="cocktail_list">
 				{error ? <div>Something went wrong</div> : null}
 				{searchedCocktails?.map((el) => (
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -8,6 +8,12 @@ import CocktailCard from "../routedApp/CocktailCard";
 import GameCard from "../routedApp/GameCard";
 import { getGameData } from "../helpers/getGameData";
 import { getCocktailData } from "../helpers/getCocktailData";
+import AlcoCocktails from "../components/AlcoCocktails";
+import NonAlcoCocktails from "../components/NonAlcoCocktails";
+import RandomCocktails from "../components/RandomCocktails";
+import { alcoholicCocktails } from "../components/myalcococktails";
+import { nonAlcoholicCocktails } from "../components/mynonalcococktails";
+import { randomCocktails } from "../components/myrandomcocktails";
 
 const ROUTES = {
 	mainPage: "/games-cocktails-app/",
@@ -15,6 +21,9 @@ const ROUTES = {
 	cocktailsListPage: "/games-cocktails-app/cocktails",
 	gamePage: "/games-cocktails-app/games/:game",
 	cocktailPage: "/games-cocktails-app/cocktails/:cocktail",
+	cocktailAlcoholic: "/games-cocktails-app/cocktails/alcoholic",
+	cocktailNonAlcoholic: "/games-cocktails-app/cocktails/non-alcoholic",
+	cocktailRandom: "/games-cocktails-app/cocktails/random",
 };
 
 export const router = createBrowserRouter([
@@ -34,6 +43,23 @@ export const router = createBrowserRouter([
 			{
 				path: ROUTES.cocktailsListPage,
 				element: <CocktailsList />,
+				children: [
+					{
+						path: ROUTES.cocktailAlcoholic,
+						element: <AlcoCocktails />,
+						loader: alcoholicCocktails,
+					},
+					{
+						path: ROUTES.cocktailNonAlcoholic,
+						element: <NonAlcoCocktails />,
+						loader: nonAlcoholicCocktails,
+					},
+					{
+						path: ROUTES.cocktailRandom,
+						element: <RandomCocktails />,
+						loader: randomCocktails,
+					},
+				],
 			},
 			{
 				path: ROUTES.gamePage,
