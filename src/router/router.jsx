6baf43b557d7b2722f5531e51d6cecbc513fa812
DimaZhuravlov <dzhv26@gmail.com>
2022-12-24 15:24:34 +0200

    add gameCard page and loaders

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,46 +1,55 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Layout from '../Layout'
-import MainPage from '../MainPage'
-import ErrorPage from '../ErrorPage'
-import GamesList from '../GamePages/GamesList'
-import CocktailsList from '../CoctailPages/CocktailsList'
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout";
+import MainPage from "../MainPage";
+import ErrorPage from "../ErrorPage";
+import GamesList from "../GamePages/GamesList";
+import CocktailsList from "../CoctailPages/CocktailsList";
+import CocktailPage from "../CoctailPages/CocktailPage";
+import GameCard from "../GamePages/GameCard";
 
-import { getCocktails } from '../CoctailPages/getCocktails'
-import { getCoctail } from '../CoctailPages/getCoctail'
-import CocktailPage from '../CoctailPages/CocktailPage'
+import { getCocktails } from "../CoctailPages/getCocktails";
+import { getCoctail } from "../CoctailPages/getCoctail";
+import { getGame } from "../GamePages/getGame";
+import { getGamesList } from "../GamePages/getGamesList";
 
 const ROUTES = {
-  mainPage: '/games-cocktails-app/',
-  gamesListPage: '/games-cocktails-app/games',
-  cocktailsListPage: '/games-cocktails-app/cocktails',
-  gamePage: '/games-cocktails-app/games/:game',
-  cocktailPage: '/games-cocktails-app/cocktail/:idDrink',
-}
+	mainPage: "/games-cocktails-app/",
+	gamesListPage: "/games-cocktails-app/games",
+	cocktailsListPage: "/games-cocktails-app/cocktails",
+	gamePage: "/games-cocktails-app/games/game/:id",
+	cocktailPage: "/games-cocktails-app/cocktail/:idDrink",
+};
 
 export const router = createBrowserRouter([
-  {
-    path: ROUTES.mainPage,
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        element: <MainPage />,
-        index: true,
-      },
-      {
-        path: ROUTES.gamesListPage,
-        element: <GamesList />,
-      },
-      {
-        path: ROUTES.cocktailsListPage,
-        element: <CocktailsList />,
-        loader: getCocktails,
-      },
-      {
-        path: ROUTES.cocktailPage,
-        element: <CocktailPage />,
-        loader: getCoctail,
-      },
-    ],
-  },
-])
+	{
+		path: ROUTES.mainPage,
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				element: <MainPage />,
+				index: true,
+			},
+			{
+				path: ROUTES.gamesListPage,
+				element: <GamesList />,
+				loader: getGamesList,
+			},
+			{
+				path: ROUTES.gamePage,
+				element: <GameCard />,
+				loader: getGame,
+			},
+			{
+				path: ROUTES.cocktailsListPage,
+				element: <CocktailsList />,
+				loader: getCocktails,
+			},
+			{
+				path: ROUTES.cocktailPage,
+				element: <CocktailPage />,
+				loader: getCoctail,
+			},
+		],
+	},
+]);
