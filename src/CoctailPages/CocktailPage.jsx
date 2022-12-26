ad15a5f8557d1c6979db20afac699ac064354378
Kyryl5 <108937818+Kyryl5@users.noreply.github.com>
2022-12-26 20:14:06 +0200

    add cocktail page content (#12)
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>

--- src/CoctailPages/CocktailPage.jsx
+++ src/CoctailPages/CocktailPage.jsx
@@ -1,18 +1,188 @@
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData } from "react-router-dom";
 
 export default function CocktailPage() {
-  const { strDrink, strDrinkThumb, strAlcoholic } = useLoaderData()
-
-  return (
-    <>
-      <h1>Cocktail:</h1>
-      <img
-        style={{ width: '200px', height: '200px' }}
-        src={strDrinkThumb}
-        alt=""
-      />
-      <p>{strDrink} cocktail</p>
-      <p>{strAlcoholic} cocktail</p>
-    </>
-  )
+	const DRINK = useLoaderData();
+	return (
+		<>
+			<h1>Cocktail: {DRINK.strDrink}</h1>
+			<div>
+				<img
+					style={{ width: "200px", height: "200px" }}
+					src={DRINK.strDrinkThumb}
+					alt=""
+				/>
+			</div>
+			<div>Category: {DRINK.strCategory}</div>
+			<p>Type of glass: {DRINK.strGlass}</p>
+			<p>{DRINK.strAlcoholic} cocktail</p>
+			<div>Instructions: {DRINK.strInstructions}</div>
+			<div className="ingredients">
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient1}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient1}</div>
+					<div>{DRINK.strMeasure1}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient2}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient2}</div>
+					<div>{DRINK.strMeasure2}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient3}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient3}</div>
+					<div>{DRINK.strMeasure3}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient4}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient4}</div>
+					<div>{DRINK.strMeasure4}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient5}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient5}</div>
+					<div>{DRINK.strMeasure5}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient6}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient6}</div>
+					<div>{DRINK.strMeasure6}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient7}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient7}</div>
+					<div>{DRINK.strMeasure7}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient8}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient8}</div>
+					<div>{DRINK.strMeasure8}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient9}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient9}</div>
+					<div>{DRINK.strMeasure9}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient10}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient10}</div>
+					<div>{DRINK.strMeasure10}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient11}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient11}</div>
+					<div>{DRINK.strMeasure11}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient12}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient12}</div>
+					<div>{DRINK.strMeasure12}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient13}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient13}</div>
+					<div>{DRINK.strMeasure13}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient14}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient14}</div>
+					<div>{DRINK.strMeasure14}</div>
+				</div>
+				<div className="ingredient">
+					<div>
+						<img
+							style={{ width: "200px", heigh: "200px" }}
+							src={`https://www.thecocktaildb.com/images/ingredients/${DRINK.strIngredient15}.png`}
+							alt=""
+						/>
+					</div>
+					<div>{DRINK.strIngredient15}</div>
+					<div>{DRINK.strMeasure15}</div>
+				</div>
+			</div>
+		</>
+	);
 }
