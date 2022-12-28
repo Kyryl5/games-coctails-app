ed483177dcd5b2e5b27547445aeeea24687302f8
Liubomyr Olianych <lubomir.olianich@gmail.com>
2022-12-29 00:01:29 +0200

    modified:   src/App.scss 	modified:   src/ErrorPage.jsx

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,12 +1,14 @@
-import { useRouteError } from 'react-router-dom'
+import { useRouteError } from "react-router-dom";
+import BrandButton from "./UiElements/BrandButton";
 
 export default function ErrorPage() {
-  const error = useRouteError()
+  const error = useRouteError();
   return (
-    <>
-      <h1>Oops!</h1>
-      <p>Sorry something went wrong</p>
-      <p>{error.statusText ?? error.message}</p>
-    </>
-  )
+    <div className="error-page">
+      <h1 className="no-result-message-h1">Oops!</h1>
+      <p>Sorry, something went wrong</p>
+      <p>Сause: {error.statusText ?? error.message}</p>
+      <BrandButton buttontext={"go home"} buttonlink={"/"} />
+    </div>
+  );
 }
