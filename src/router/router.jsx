c5a64c078f8559cf2ef883ea48c07552e10c3eb8
Kyryl5 <108937818+Kyryl5@users.noreply.github.com>
2022-12-28 16:25:32 +0200

    Develop (#29)
    * Fetch data (#1)
    
    * first commit
    
    * first step
    
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>
    
    * add loaders (#2)
    
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>
    
    * Url params (#3)
    
    * routing coctails structure
    
    * remove button search
    
    Co-authored-by: Kyryl Savytskyi <kirill_savitsky56@gmail.com>
    
    * add games photo and fetch games from db (#4)
    
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>
    
    * add games photo and fetch games from db (#5)
    
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>
    
    * add gameCard page and loaders (#6)
    
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>
    
    * fix find coctails (#8)
    
    Co-authored-by: Kyryl Savytskyi <kirill_savitsky56@gmail.com>
    
    * starting work on the project design (#9)
    
    * add-random-coctail-btn (#10)
    
    Co-authored-by: Kyryl Savytskyi <kirill_savitsky56@gmail.com>
    
    * add-random-coctail-btn (#11)
    
    Co-authored-by: Kyryl Savytskyi <kirill_savitsky56@gmail.com>
    
    * add cocktail page content (#12)
    
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>
    
    * fix-searching-input (#13)
    
    Co-authored-by: Kyryl Savytskyi <kirill_savitsky56@gmail.com>
    
    * footer, games list, game & coctails pages design (#14)
    
    * fix-search-input (#15)
    
    Co-authored-by: Kyryl Savytskyi <kirill_savitsky56@gmail.com>
    
    * add random game and cocktail (#16)
    
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>
    
    * ScrollRestoration, logo path and prettierrc upd
    
    * modified:   src/MainPage.jsx
    
    * modified:   src/MainPage.jsx
    
    * modified:   src/MainPage.jsx
    	modified:   src/UiElements/BrandButton.jsx
    
    * modified:   src/CoctailPages/CocktailsList.jsx
    	modified:   src/GamePages/GamesList.jsx
    
    * modified:   src/GamePages/GameCard.jsx
    
    * modified:   index.html
    
    * modified:   index.html
    
    * modified:   index.html
    
    * add scroll and game filter
    
    * new file:   src/UiElements/GoToTopButton.jsx (#26)
    
    * fix-naming-routs (#28)
    
    Co-authored-by: Kyryl Savytskyi <kirill_savitsky56@gmail.com>
    
    Co-authored-by: DimaZhuravlov <dzhv26@gmail.com>
    Co-authored-by: Kyryl Savytskyi <kirill_savitsky56@gmail.com>
    Co-authored-by: Liubomyr <Lubomir.olianich@gmail.com>
    Co-authored-by: DimaZHV <101501143+DimaZHV@users.noreply.github.com>

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,28 +1,24 @@
-import { createBrowserRouter } from "react-router-dom";
-import Layout from "../Layout";
-import MainPage from "../MainPage";
-import ErrorPage from "../ErrorPage";
-import GamesList from "../GamePages/GamesList";
-import CocktailsList from "../CoctailPages/CocktailsList";
-import CocktailPage from "../CoctailPages/CocktailPage";
-import RandomCocktailPage from "../CoctailPages/RandomCocktailPage";
-import GameCard from "../GamePages/GameCard";
-// import SearchedCoctailsPage from '../CoctailPages/SearchedCoctailsPage'
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from '../Layout'
+import MainPage from '../MainPage'
+import ErrorPage from '../ErrorPage'
+import GamesList from '../GamePages/GamesList'
+import CocktailsList from '../CoctailPages/CocktailsList'
+import CocktailPage from '../CoctailPages/CocktailPage'
+import GameCard from '../GamePages/GameCard'
 
-import { getCocktails } from "../CoctailPages/getCocktails";
-import { getCoctail } from "../CoctailPages/getCoctail";
-import { getRandomCoctail } from "../CoctailPages/getRandomCoctail";
-import { getGame } from "../GamePages/getGame";
-import { getGamesList } from "../GamePages/getGamesList";
+import { getCocktails } from '../CoctailPages/getCocktails'
+import { getCoctail } from '../CoctailPages/getCoctail'
+import { getGame } from '../GamePages/getGame'
+import { getGamesList } from '../GamePages/getGamesList'
 
 export const ROUTES = {
-  mainPage: "/games-cocktails-app/",
-  gamesListPage: "/games-cocktails-app/games",
-  cocktailsListPage: "/games-cocktails-app/cocktails",
-  gamePage: "/games-cocktails-app/games/game/:id",
-  cocktailPage: "/games-cocktails-app/cocktail/:idDrink",
-  randomCocktailPage: "/games-cocktails-app/cocktail/random",
-};
+  mainPage: '/games-coctails-app/',
+  gamesListPage: '/games-coctails-app/games',
+  cocktailsListPage: '/games-coctails-app/cocktails',
+  gamePage: '/games-coctails-app/games/game/:id',
+  cocktailPage: '/games-coctails-app/cocktail/:idDrink',
+}
 
 export const router = createBrowserRouter([
   {
@@ -54,11 +50,6 @@ export const router = createBrowserRouter([
         element: <CocktailPage />,
         loader: getCoctail,
       },
-      {
-        path: ROUTES.randomCocktailPage,
-        element: <RandomCocktailPage />,
-        loader: getRandomCoctail,
-      },
     ],
   },
-]);
+])
