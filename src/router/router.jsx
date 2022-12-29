20a537c15d9d29b80a2cdd555a8bf45961131a07
Kyryl5 <108937818+Kyryl5@users.noreply.github.com>
2022-12-29 14:18:12 +0200

    fix-autocomplete-input (#50)
    Co-authored-by: Kyryl Savytskyi <kirill_savitsky56@gmail.com>

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,64 +1,57 @@
-import { createHashRouter } from "react-router-dom";
+import { createHashRouter } from 'react-router-dom'
 
-import Layout from "../Layout";
-import MainPage from "../MainPage";
-import ErrorPage from "../ErrorPage";
-import GamesList from "../GamePages/GamesList";
-import CocktailsList from "../CoctailPages/CocktailsList";
-import CocktailPage from "../CoctailPages/CocktailPage";
-import GamePage from "../GamePages/GamePage";
+import Layout from '../Layout'
+import MainPage from '../MainPage'
+import ErrorPage from '../ErrorPage'
+import GamesList from '../GamePages/GamesList'
+import CocktailsList from '../CoctailPages/CocktailsList'
+import CocktailPage from '../CoctailPages/CocktailPage'
+import GamePage from '../GamePages/GamePage'
 
-import { getCocktails } from "../CoctailPages/getCocktailsList";
-import { getCoctail } from "../CoctailPages/getCoctail";
-import { getGame } from "../GamePages/getGame";
-import { getGamesList } from "../GamePages/getGamesList";
+import { getCocktails } from '../CoctailPages/getCocktailsList'
+import { getCoctail } from '../CoctailPages/getCoctail'
+import { getGame } from '../GamePages/getGame'
+import { getGamesList } from '../GamePages/getGamesList'
 
-// export const ROUTES = {
-// 	mainPage: "/games-coctails-app/",
-// 	gamesListPage: "/games-coctails-app/games",
-// 	cocktailsListPage: "/games-coctails-app/cocktails",
-// 	gamePage: "/games-coctails-app/games/game/:id",
-// 	cocktailPage: "/games-coctails-app/cocktail/:idDrink",
-// };
 export const ROUTES = {
-	mainPage: "/",
-	gamesListPage: "/games",
-	cocktailsListPage: "/cocktails",
-	gamePage: "/games/game/:id",
-	cocktailPage: "/cocktail/:idDrink",
-};
+  mainPage: '/',
+  gamesListPage: '/games',
+  cocktailsListPage: '/cocktails',
+  gamePage: '/games/game/:id',
+  cocktailPage: '/cocktail/:idDrink',
+}
 
 export const router = createHashRouter([
-	{
-		path: ROUTES.mainPage,
-		element: <Layout />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				element: <MainPage />,
-				index: true,
-			},
-			{
-				path: ROUTES.gamesListPage,
-				element: <GamesList />,
-				loader: getGamesList,
-			},
-			{
-				path: ROUTES.gamePage,
-				element: <GamePage />,
-				errorElement: <ErrorPage />,
-				loader: getGame,
-			},
-			{
-				path: ROUTES.cocktailsListPage,
-				element: <CocktailsList />,
-				loader: getCocktails,
-			},
-			{
-				path: ROUTES.cocktailPage,
-				element: <CocktailPage />,
-				loader: getCoctail,
-			},
-		],
-	},
-]);
+  {
+    path: ROUTES.mainPage,
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        element: <MainPage />,
+        index: true,
+      },
+      {
+        path: ROUTES.gamesListPage,
+        element: <GamesList />,
+        loader: getGamesList,
+      },
+      {
+        path: ROUTES.gamePage,
+        element: <GamePage />,
+        errorElement: <ErrorPage />,
+        loader: getGame,
+      },
+      {
+        path: ROUTES.cocktailsListPage,
+        element: <CocktailsList />,
+        loader: getCocktails,
+      },
+      {
+        path: ROUTES.cocktailPage,
+        element: <CocktailPage />,
+        loader: getCoctail,
+      },
+    ],
+  },
+])
