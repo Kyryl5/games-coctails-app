ef9764452bed123214b7a7789dfd59c5a90a0cdc
DimaZhuravlov <dzhv26@gmail.com>
2022-12-29 09:24:22 +0200

    add some error handlers

--- src/GamePages/getGamesList.jsx
+++ src/GamePages/getGamesList.jsx
@@ -1,5 +1,10 @@
 export const getGamesList = ({ params }) => {
-	return fetch(
-		"https://my-json-server.typicode.com/DimaZHV/cheers-db/games"
-	).then((resp) => resp.json());
+	return fetch("https://my-json-server.typicode.com/DimaZHV/cheers-db/games")
+		.then((resp) => {
+			if (!resp.ok) {
+				throw Error("We have an error but we didn't have time to handle it:(");
+			}
+			return resp.json();
+		})
+		.catch((error) => console.log("Something happened! " + error.message));
 };
